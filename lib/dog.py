aaeac4eb9454950da7e8e1ba7a983c9662431967
Yael Harosh <yaelharosh01@gmail.com>
2023-04-04 09:31:16 -0400

    one pass test

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,14 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def set_name(self, name):
+        if(type(str)) and (len(name) >= 1 and len(name) <= 25):
+            self._name = name
+        else:
+            print('Name must be string between 1 and 25 characters.')
+
+    name = property(set_name)
+
+# bob = Dog()
+# bob.name = 'bob'
+# print(bob.name)
