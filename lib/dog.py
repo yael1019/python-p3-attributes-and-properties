39f379a4abba6bc87409172950e47cf9636af00d
Yael Harosh <yaelharosh01@gmail.com>
2023-04-04 14:30:07 -0400

    still no pass

--- lib/dog.py
+++ lib/dog.py
@@ -12,8 +12,12 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
+    # def __init__(self, name = 'fido'):
+    #     self.name = name
+    def get_name(self):
+        return self.name
     def set_name(self, name):
-        if(type(str)) and (len(name) >= 1 and len(name) <= 25):
+        if(isinstance(name, str)) and (len(name) >= 1 and len(name) <= 25):
             self._name = name
         else:
             print('Name must be string between 1 and 25 characters.')
